ed73667b2fe1dbadadcbbe118ad03ea6722a36af
Midkard <dimigam@yandex.ru>
2022-01-11 20:20:55 +0300

    1.4.1 Label-none fix for initially filled

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,9 +1,9 @@
 $(function(){
     $('form').validate({
-        hideLabels: true,
-        validateOnBlur: true,
-        privacy: '/123',
-        phoneRegex : /^\+7\([023456789]\d{2}\) \d{3}-\d{2}-\d{2}$/,
+        alphaRegex: /^.*$/i,
+        messages: {
+            alpha: '',
+        },
     });
 });
 
